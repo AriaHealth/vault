df123472348e626b5138ef0ff2f6c6b177055e69
linerocks <tirtadwipa.manunggal@gmail.com>
2022-11-07 10:56:49 +0700

    set asg desired capacity

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,9 +44,9 @@ Resources:
     Properties:
       AutoScalingGroupName: "${var.name}"
       HealthCheckGracePeriod: 300
-      DesiredCapacity: 0
-      MaxSize: 0
-      MinSize: 0
+      DesiredCapacity: 1
+      MaxSize: 1
+      MinSize: 1
       VPCZoneIdentifier: ["${join("\",\"", data.aws_subnet_ids.all.ids)}"]
       TargetGroupARNs: ${jsonencode([for tg_arn in module.alb.target_group_arns : tg_arn])}
       Tags: ${jsonencode([for key, value in var.tags : map("Key", key, "PropagateAtLaunch", "true", "Value", value)])}
